2e1a5aa39e09283d22539966dcc60f0478461f32
d4Rk <darkdevilx@gmx.net>
2021-02-26 08:42:35 +0100

    initial core data setup

--- timeARC/iOS/App.swift
+++ timeARC/iOS/App.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 import SwiftUIFlux
+import CoreData
 
 @main
 struct app: App {
+    let cloudKitContainer = NSPersistentCloudKitContainer(name: "timeARC")
     let watchCommunicator = WatchCommunicator(dispatch: store.dispatch)
 
     init() {
@@ -17,6 +19,32 @@ struct app: App {
             print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         #endif
 
+        self.initCoreData()
+        self.initStore()
+    }
+
+    private func initCoreData() {
+//        try? self.cloudKitContainer.initializeCloudKitSchema() // TODO: Remove before shipping the app?
+
+        self.cloudKitContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                Typical reasons for an error here include:
+                * The parent directory does not exist, cannot be created, or disallows writing.
+                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                * The device is out of space.
+                * The store could not be migrated to the current model version.
+                Check the error message to determine what the actual problem was.
+                */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+    }
+
+    private func initStore() {
         store.dispatch(action: InitFlux())
 
         store.dispatch(action: AppStateLoadingInProgress())
